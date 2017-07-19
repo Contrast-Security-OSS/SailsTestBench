1cf03bd9d382b7eb39fbb2f875af4ec92c3a2bd9
Ehden Sinai <ehden.sinai@contrastsecurity.com>
2017-07-19 13:16:21 -0400

    CONTRAST-15178

--- api/controllers/XssController.js
+++ api/controllers/XssController.js
@@ -7,39 +7,66 @@
 
 module.exports = {
     index: function (req, res) {
-    
+
         return res.view();
     },
-    
+
     post: function (req, res) {
 		res.set('X-XSS-Protection', '0'); // disable browser xss protection for chrome
-		// taint user input 
+		// taint user input
 		var input = req.body.input;
-		
+
 		var output = '<html>input: ' + input + '</html>';
-			// this should trigger XSS 
+			// this should trigger XSS
 		res.send(output);
     },
-    
+    post_protect: function (req, res) {
+		res.set('X-XSS-Protection', '0'); // disable browser xss protection for chrome
+		// taint user input
+		var input = req.body.input;
+
+		var output = '<html>input: ' + input + '</html>';
+			// this should trigger XSS
+		res.send(output);
+    },
+
     reflected: function (req, res) {
 		res.set('X-XSS-Protection', '0'); // disable browser xss protection for chrome
-		// taint user input 
+		// taint user input
 		var input = req.query.input;
-		
+		console.log(input.__contrastProperties);
+
 		var output = '<html>input: ' + input + '</html>';
-			// this should trigger XSS 
+			// this should trigger XSS
 		res.send(output);
     },
-    
+    reflected_protect: function (req, res) {
+		res.set('X-XSS-Protection', '0'); // disable browser xss protection for chrome
+		// taint user input
+		var input = req.query.input;
+
+		var output = '<html>input: ' + input + '</html>';
+			// this should trigger XSS
+		res.send(output);
+    },
+
     safe: function (req, res) {
 		res.set('X-XSS-Protection', '0'); // disable browser xss protection for chrome
-		// taint user input 
+		// taint user input
 		var input = escape(req.query.input);
 		var output = '<html>input: ' + input + '</html>';
-			// this should trigger XSS 
+			// this should trigger XSS
+		res.send(output);
+    },
+    safe_protect: function (req, res) {
+		res.set('X-XSS-Protection', '0'); // disable browser xss protection for chrome
+		// taint user input
+		var input = escape(req.query.input);
+		var output = '<html>input: ' + input + '</html>';
+			// this should trigger XSS
 		res.send(output);
     }
-    
-    
+
+
 };
 
